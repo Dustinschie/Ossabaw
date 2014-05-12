a755f90766591f62ee8baf3fed7b9b3b708cc4f8
Dustinschie <dustinschie@gmail.com>
2014-05-12 13:36:26 -0400

    new UI

--- Ossabaw/JournalViewController.m
+++ Ossabaw/JournalViewController.m
@@ -31,12 +31,14 @@
 
 - (void) viewWillAppear:(BOOL)animated
 {
+    [[self navigationController] setNavigationBarHidden:NO animated:YES];
     [super viewDidAppear:animated];
     [super viewWillAppear:animated];
     [[[self tabBarController] tabBar] setHidden:YES];
     [[self textView] setTextColor:[UIColor whiteColor]];
     if ([self journal] != nil) {
         [self setTitle:[[self journal] title]];
+        
         [[self textView] setText:[[self journal] information]];
         int num_of_photos = [[[self journal] photos] count],
         width =[[self scrollView] frame].size.width,
@@ -92,7 +94,7 @@
 
 - (void) viewDidAppear:(BOOL)animated
 {
-        NSLog(@"will apear");
+        [super viewDidAppear:animated];
 
 }
 
